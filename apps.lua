0d5bbebd400c2950dae4f5d9b82e0f21305d2d3f
Bublade <alexthvb@gmail.com>
2025-12-02 23:38:12 +0100

    changed colors added more apps

--- apps.lua
+++ apps.lua
@@ -1,5 +1,6 @@
-local powershell = { icon = "󰨊", color = "#4d87ec" }
+local powershell = { icon = "󰨊", color = "#57923d" }
 local neovim = { icon = "", color = "#00b952" }
+local sh = { icon = "", color = "#47b353"}
 return {
 	nvim = neovim,
 	neovim = neovim,
@@ -8,4 +9,8 @@ return {
 	wsl = { icon = "", color = "#d9a511" },
 	lazygit = { icon = "󰊢", color = "#f05133" },
 	ssh = { icon = "", color = "#fcd058" },
+	bash = { icon = "", color = "#47b353"},
+	zsh = { icon = "%_", color = "#f15a24"},
+	nu = { icon = "nu", color = "#4e9a06"}
 }
+
